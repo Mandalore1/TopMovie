6697a355a9157d5ab8f42c573b205c18aff9f899
Mandalore <serriy_volk@mail.ru>
2020-08-24 18:37:52 +0500

    Add parse_additional_info for IMDBDetailParser

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from parsers.imdb import IMDBParser, IMDBDetailParser
 
-LIST_URL = "https://www.imdb.com/search/title/?title_type=feature&num_votes=25000,&sort=user_rating,desc"
-DETAIL_URL = "https://www.imdb.com/title/tt0111161/"
+LIST_URL = "https://www.imdb.com/search/title/?title_type=feature&sort=user_rating,desc"
+DETAIL_URL = "https://www.imdb.com/title/tt0252487/?ref_=adv_li_tt"
 
-parser = IMDBParser(LIST_URL)
-
-movies = parser.parse()
-
-for movie in movies:
-    print(movie, "\n")
-
-# parser = IMDBDetailParser(DETAIL_URL)
+# parser = IMDBParser(LIST_URL)
 #
-# movie = parser.parse()
+# movies = parser.parse()
 #
-# print(movie, "\n", movie.details)
+# for movie in movies:
+#     print(movie, "\n")
+
+parser = IMDBDetailParser(DETAIL_URL)
+
+movie = parser.parse()
+
+print(movie, "\n", movie.details)
--- parsers/imdb.py
+++ parsers/imdb.py
@@ -18,7 +18,10 @@ class IMDBParser(MovieParser):
         return movie.find("h3").find("a").get_text(strip=True)
 
     def parse_year(self, movie):
-        return int(movie.find("span", class_="lister-item-year").get_text(strip=True).strip("()"))
+        result = movie.find("span", class_="lister-item-year").get_text(strip=True)
+        # Remove all non-digits
+        result = "".join([i for i in result if i.isdigit()])
+        return int(result)
 
     def parse_rating(self, movie):
         return float(movie.find("div", class_="ratings-imdb-rating").get_text(strip=True))
@@ -50,5 +53,54 @@ class IMDBDetailParser(MovieDetailParser):
 
     def parse_additional_info(self, soup):
         details = dict()
+        details["poster_url"] = soup.select_one("div.poster img")["src"]
         details["ratings_count"] = int(soup.find("span", itemprop="ratingCount").get_text(strip=True).replace(",", ""))
+
+        # Add time
+        time = soup.select_one("div.subtext time")
+        if time:
+            details["time"] = time.get_text(strip=True)
+
+        # Add genres
+        # Release date may also select to genres
+        details["genres"] = [i.get_text(strip=True) for i in soup.select("div.subtext a")]
+
+        # Add release date
+        # Pop release date from genres
+        try:
+            if not details["genres"][-1].isalpha():  # Genre can not contain digit
+                details["release_date"] = details["genres"].pop()
+        except IndexError:  # If release date and genres does not exist on page
+            pass
+
+        # Add credits
+        credits = soup.find_all("div", class_="credit_summary_item")
+
+        # Function to add credit info to details
+        def add_credit(credit, name, details):
+           details[name] = [i.get_text(strip=True) for i in credit.find_all("a") if i["href"].startswith("/name")]
+
+        for credit in credits:
+            text = credit.get_text(strip=True)
+            if text.find("Director") != -1:
+                add_credit(credit, "directors", details)
+            elif text.find("Writer") != -1:
+                add_credit(credit, "writers", details)
+            elif text.find("Star") != -1:
+                add_credit(credit, "stars", details)
+
+        # Add storyline
+        storyline = soup.select_one("#titleStoryLine p")
+        if storyline:
+            details["storyline"] = storyline.get_text(strip=True)
+
+        txt_blocks = soup.select("div.txt-block")
+        # Add budget and worldwide gross
+        for block in txt_blocks:
+            text = block.get_text(strip=True)
+            if text.find("Budget") != -1:
+                details["budget"] = "".join([i for i in text if i.isdigit()])
+            if text.find("Cumulative Worldwide Gross") != -1:
+                details["worldwide_gross"] = "".join([i for i in text if i.isdigit()])
+
         return details
